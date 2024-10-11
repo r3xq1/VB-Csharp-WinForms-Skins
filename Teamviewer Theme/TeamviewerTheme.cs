53914e2d01848d1c8c89a0454d3f24d6ce88619a
r3xq1 <79271705+r3xq1@users.noreply.github.com>
2024-10-11 21:39:41 +0300

    Add files via upload

---
+++ Teamviewer Theme/TeamviewerTheme.cs
@@ -0,0 +1,534 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+public abstract class Theme : ContainerControl
+{
+    protected Graphics G;
+    private bool ParentIsForm;
+    private Color _TransparencyKey;
+    private Image _Image;
+    protected Rectangle Header;
+    private bool _Resizable = true;
+    private int _MoveHeight = 24;
+
+    public Theme()
+    {
+        SetStyle((ControlStyles)139270, true);
+    }
+
+    protected override void OnHandleCreated(EventArgs e)
+    {
+        Dock = DockStyle.Fill;
+        ParentIsForm = Parent is Form;
+
+        if (ParentIsForm)
+        {
+            if (_TransparencyKey != Color.Empty)
+                ParentForm.TransparencyKey = _TransparencyKey;
+            ParentForm.FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+
+        base.OnHandleCreated(e);
+    }
+
+    public new string Text
+    {
+        get => base.Text;
+        set
+        {
+            base.Text = value;
+            Invalidate();
+        }
+    }
+
+    public bool Resizable
+    {
+        get => _Resizable;
+        set => _Resizable = value;
+    }
+
+    public int MoveHeight
+    {
+        get => _MoveHeight;
+        set
+        {
+            _MoveHeight = value;
+            Header = new Rectangle(7, 7, Width - 14, _MoveHeight - 7);
+        }
+    }
+
+    protected override void OnMouseDown(MouseEventArgs e)
+    {
+        if (e.Button != MouseButtons.Left) return;
+        if (ParentIsForm && ParentForm.WindowState == FormWindowState.Maximized) return;
+
+        IntPtr flag;
+
+        if (Header.Contains(e.Location))
+        {
+            flag = new IntPtr(2); // Dragging area
+        }
+        else if (Current.Position == 0 || !_Resizable)
+        {
+            return; // No action allowed
+        }
+        else
+        {
+            flag = new IntPtr(Current.Position); // Resizing area
+        }
+
+        Capture = false;
+
+        // Изменяем строку на использование нового Message
+        var msg = Message.Create(Handle, 0xA1, flag, IntPtr.Zero);
+        DefWndProc(ref msg); // Передаем по ссылке
+
+        base.OnMouseDown(e);
+    }
+
+
+
+
+
+
+
+    private struct Pointer
+    {
+        public Cursor Cursor { get; }
+        public byte Position { get; }
+
+        public Pointer(Cursor c, byte p)
+        {
+            Cursor = c;
+            Position = p;
+        }
+    }
+
+    private Pointer Current, Pending;
+    private Point PTC;
+
+    private Pointer GetPointer()
+    {
+        PTC = PointToClient(MousePosition);
+
+        bool F1 = PTC.X < 7;
+        bool F2 = PTC.X > Width - 7;
+        bool F3 = PTC.Y < 7;
+        bool F4 = PTC.Y > Height - 7;
+
+        if (F1 && F3) return new Pointer(Cursors.SizeNWSE, 13);
+        if (F1 && F4) return new Pointer(Cursors.SizeNESW, 16);
+        if (F2 && F3) return new Pointer(Cursors.SizeNESW, 14);
+        if (F2 && F4) return new Pointer(Cursors.SizeNWSE, 17);
+        if (F1) return new Pointer(Cursors.SizeWE, 10);
+        if (F2) return new Pointer(Cursors.SizeWE, 11);
+        if (F3) return new Pointer(Cursors.SizeNS, 12);
+        if (F4) return new Pointer(Cursors.SizeNS, 15);
+
+        return new Pointer(Cursors.Default, 0);
+    }
+
+    private void SetCurrent()
+    {
+        Pending = GetPointer();
+        if (Current.Position == Pending.Position) return;
+
+        Current = Pending;
+        Cursor = Current.Cursor;
+    }
+
+    protected override void OnMouseMove(MouseEventArgs e)
+    {
+        if (_Resizable) SetCurrent();
+        base.OnMouseMove(e);
+    }
+
+    protected override void OnSizeChanged(EventArgs e)
+    {
+        if (Width == 0 || Height == 0) return;
+        Header = new Rectangle(7, 7, Width - 14, _MoveHeight - 7);
+        Invalidate();
+        base.OnSizeChanged(e);
+    }
+
+    public abstract void PaintHook();
+
+    protected sealed override void OnPaint(PaintEventArgs e)
+    {
+        if (Width == 0 || Height == 0) return;
+        G = e.Graphics;
+        PaintHook();
+    }
+
+    public Color TransparencyKey
+    {
+        get => _TransparencyKey;
+        set
+        {
+            _TransparencyKey = value;
+            Invalidate();
+        }
+    }
+
+    public Image Image
+    {
+        get => _Image;
+        set
+        {
+            _Image = value;
+            Invalidate();
+        }
+    }
+
+    public int ImageWidth => _Image?.Width ?? 0;
+
+    private Size _Size;
+    private Rectangle _Rectangle;
+    private LinearGradientBrush _Gradient;
+    private SolidBrush _Brush;
+
+    protected void DrawCorners(Color c, Rectangle rect)
+    {
+        _Brush = new SolidBrush(c);
+        G.FillRectangle(_Brush, rect.X, rect.Y, 1, 1);
+        G.FillRectangle(_Brush, rect.X + (rect.Width - 1), rect.Y, 1, 1);
+        G.FillRectangle(_Brush, rect.X, rect.Y + (rect.Height - 1), 1, 1);
+        G.FillRectangle(_Brush, rect.X + (rect.Width - 1), rect.Y + (rect.Height - 1), 1, 1);
+    }
+
+    protected void DrawBorders(Pen p1, Pen p2, Rectangle rect)
+    {
+        G.DrawRectangle(p1, rect.X, rect.Y, rect.Width - 1, rect.Height - 1);
+        G.DrawRectangle(p2, rect.X + 1, rect.Y + 1, rect.Width - 3, rect.Height - 3);
+    }
+
+    protected void DrawText(HorizontalAlignment a, Color c, int x, int y = 0)
+    {
+        if (string.IsNullOrEmpty(Text)) return;
+        _Size = TextRenderer.MeasureText(Text, Font);
+        _Brush = new SolidBrush(c);
+
+        switch (a)
+        {
+            case HorizontalAlignment.Left:
+                G.DrawString(Text, Font, _Brush, x, _MoveHeight / 2 - _Size.Height / 2 + y);
+                break;
+            case HorizontalAlignment.Right:
+                G.DrawString(Text, Font, _Brush, Width - _Size.Width - x, _MoveHeight / 2 - _Size.Height / 2 + y);
+                break;
+            case HorizontalAlignment.Center:
+                G.DrawString(Text, Font, _Brush, Width / 2 - _Size.Width / 2 + x, _MoveHeight / 2 - _Size.Height / 2 + y);
+                break;
+        }
+    }
+
+    protected void DrawIcon(HorizontalAlignment a, int x, int y = 0)
+    {
+        if (_Image == null) return;
+
+        switch (a)
+        {
+            case HorizontalAlignment.Left:
+                G.DrawImage(_Image, x, _MoveHeight / 2 - _Image.Height / 2 + y);
+                break;
+            case HorizontalAlignment.Right:
+                G.DrawImage(_Image, Width - _Image.Width - x, _MoveHeight / 2 - _Image.Height / 2 + y);
+                break;
+            case HorizontalAlignment.Center:
+                G.DrawImage(_Image, Width / 2 - _Image.Width / 2, _MoveHeight / 2 - _Image.Height / 2);
+                break;
+        }
+    }
+
+    protected void DrawGradient(Color c1, Color c2, int x, int y, int width, int height, float angle)
+    {
+        _Rectangle = new Rectangle(x, y, width, height);
+        _Gradient = new LinearGradientBrush(_Rectangle, c1, c2, angle);
+        G.FillRectangle(_Gradient, _Rectangle);
+    }
+}
+
+public abstract class ThemeControl : Control
+{
+    protected Graphics G;
+    protected Bitmap B;
+    private State MouseState;
+
+    public ThemeControl()
+    {
+        SetStyle((ControlStyles)139270, true);
+        B = new Bitmap(1, 1);
+        G = Graphics.FromImage(B);
+    }
+
+    public void AllowTransparent()
+    {
+        SetStyle(ControlStyles.Opaque, false);
+        SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+    }
+
+    public new string Text
+    {
+        get => base.Text;
+        set
+        {
+            base.Text = value;
+            Invalidate();
+        }
+    }
+
+    private enum State : byte
+    {
+        MouseNone = 0,
+        MouseOver = 1,
+        MouseDown = 2
+    }
+
+    protected override void OnMouseLeave(EventArgs e)
+    {
+        ChangeMouseState(State.MouseNone);
+        base.OnMouseLeave(e);
+    }
+
+    protected override void OnMouseEnter(EventArgs e)
+    {
+        ChangeMouseState(State.MouseOver);
+        base.OnMouseEnter(e);
+    }
+
+    protected override void OnMouseUp(MouseEventArgs e)
+    {
+        ChangeMouseState(State.MouseOver);
+        base.OnMouseUp(e);
+    }
+
+    protected override void OnMouseDown(MouseEventArgs e)
+    {
+        if (e.Button == MouseButtons.Left)
+            ChangeMouseState(State.MouseDown);
+        base.OnMouseDown(e);
+    }
+
+    private void ChangeMouseState(State e)
+    {
+        MouseState = e;
+        Invalidate();
+    }
+
+    public abstract void PaintHook();
+
+    protected sealed override void OnPaint(PaintEventArgs e)
+    {
+        if (Width == 0 || Height == 0) return;
+        PaintHook();
+        e.Graphics.DrawImage(B, 0, 0);
+    }
+
+    protected override void OnSizeChanged(EventArgs e)
+    {
+        if (Width != 0 && Height != 0)
+        {
+            B = new Bitmap(Width, Height);
+            G = Graphics.FromImage(B);
+            Invalidate();
+        }
+        base.OnSizeChanged(e);
+    }
+
+    private bool _NoRounding;
+    public bool NoRounding
+    {
+        get => _NoRounding;
+        set
+        {
+            _NoRounding = value;
+            Invalidate();
+        }
+    }
+
+    private Image _Image;
+    public Image Image
+    {
+        get => _Image;
+        set
+        {
+            _Image = value;
+            Invalidate();
+        }
+    }
+
+    public int ImageWidth => _Image?.Width ?? 0;
+
+    public int ImageTop => _Image == null ? 0 : Height / 2 - _Image.Height / 2;
+
+    private Size _Size;
+    private Rectangle _Rectangle;
+    private LinearGradientBrush _Gradient;
+    private SolidBrush _Brush;
+
+    protected void DrawCorners(Color c, Rectangle rect)
+    {
+        if (_NoRounding) return;
+
+        B.SetPixel(rect.X, rect.Y, c);
+        B.SetPixel(rect.X + (rect.Width - 1), rect.Y, c);
+        B.SetPixel(rect.X, rect.Y + (rect.Height - 1), c);
+        B.SetPixel(rect.X + (rect.Width - 1), rect.Y + (rect.Height - 1), c);
+    }
+
+    protected void DrawBorders(Pen p1, Pen p2, Rectangle rect)
+    {
+        G.DrawRectangle(p1, rect.X, rect.Y, rect.Width - 1, rect.Height - 1);
+        G.DrawRectangle(p2, rect.X + 1, rect.Y + 1, rect.Width - 3, rect.Height - 3);
+    }
+
+    protected void DrawText(HorizontalAlignment a, Color c, int x, int y = 0)
+    {
+        if (string.IsNullOrEmpty(Text)) return;
+        _Size = TextRenderer.MeasureText(Text, Font);
+        _Brush = new SolidBrush(c);
+
+        switch (a)
+        {
+            case HorizontalAlignment.Left:
+                G.DrawString(Text, Font, _Brush, x, Height / 2 - _Size.Height / 2 + y);
+                break;
+            case HorizontalAlignment.Right:
+                G.DrawString(Text, Font, _Brush, Width - _Size.Width - x, Height / 2 - _Size.Height / 2 + y);
+                break;
+            case HorizontalAlignment.Center:
+                G.DrawString(Text, Font, _Brush, Width / 2 - _Size.Width / 2 + x, Height / 2 - _Size.Height / 2 + y);
+                break;
+        }
+    }
+
+    protected void DrawIcon(HorizontalAlignment a, int x, int y = 0)
+    {
+        if (_Image == null) return;
+
+        switch (a)
+        {
+            case HorizontalAlignment.Left:
+                G.DrawImage(_Image, x, Height / 2 - _Image.Height / 2 + y);
+                break;
+            case HorizontalAlignment.Right:
+                G.DrawImage(_Image, Width - _Image.Width - x, Height / 2 - _Image.Height / 2 + y);
+                break;
+            case HorizontalAlignment.Center:
+                G.DrawImage(_Image, Width / 2 - _Image.Width / 2, Height / 2 - _Image.Height / 2);
+                break;
+        }
+    }
+
+    protected void DrawGradient(Color c1, Color c2, int x, int y, int width, int height, float angle)
+    {
+        _Rectangle = new Rectangle(x, y, width, height);
+        _Gradient = new LinearGradientBrush(_Rectangle, c1, c2, angle);
+        G.FillRectangle(_Gradient, _Rectangle);
+    }
+}
+
+public abstract class ThemeContainerControl : ContainerControl
+{
+    protected Graphics G;
+    protected Bitmap B;
+
+    public ThemeContainerControl()
+    {
+        SetStyle((ControlStyles)139270, true);
+        B = new Bitmap(1, 1);
+        G = Graphics.FromImage(B);
+    }
+
+    public void AllowTransparent()
+    {
+        SetStyle(ControlStyles.Opaque, false);
+        SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+    }
+
+    public abstract void PaintHook();
+
+    protected sealed override void OnPaint(PaintEventArgs e)
+    {
+        if (Width == 0 || Height == 0) return;
+        PaintHook();
+        e.Graphics.DrawImage(B, 0, 0);
+    }
+
+    protected override void OnSizeChanged(EventArgs e)
+    {
+        if (Width != 0 && Height != 0)
+        {
+            B = new Bitmap(Width, Height);
+            G = Graphics.FromImage(B);
+            Invalidate();
+        }
+        base.OnSizeChanged(e);
+    }
+
+    private bool _NoRounding;
+    public bool NoRounding
+    {
+        get => _NoRounding;
+        set
+        {
+            _NoRounding = value;
+            Invalidate();
+        }
+    }
+
+    private Rectangle _Rectangle;
+    private LinearGradientBrush _Gradient;
+
+    protected void DrawCorners(Color c, Rectangle rect)
+    {
+        if (_NoRounding) return;
+
+        B.SetPixel(rect.X, rect.Y, c);
+        B.SetPixel(rect.X + (rect.Width - 1), rect.Y, c);
+        B.SetPixel(rect.X, rect.Y + (rect.Height - 1), c);
+        B.SetPixel(rect.X + (rect.Width - 1), rect.Y + (rect.Height - 1), c);
+    }
+
+    protected void DrawBorders(Pen p1, Pen p2, Rectangle rect)
+    {
+        G.DrawRectangle(p1, rect.X, rect.Y, rect.Width - 1, rect.Height - 1);
+        G.DrawRectangle(p2, rect.X + 1, rect.Y + 1, rect.Width - 3, rect.Height - 3);
+    }
+
+    protected void DrawGradient(Color c1, Color c2, int x, int y, int width, int height, float angle)
+    {
+        _Rectangle = new Rectangle(x, y, width, height);
+        _Gradient = new LinearGradientBrush(_Rectangle, c1, c2, angle);
+        G.FillRectangle(_Gradient, _Rectangle);
+    }
+}
+
+public class TeamViewerTheme : Theme
+{
+    public TeamViewerTheme()
+    {
+        MoveHeight = 19;
+        // TransparencyKey = Color.Fuchsia;
+        // Me.Resizable = false;
+        Font = new Font("Microsoft Sans Serif", 9, FontStyle.Regular);
+    }
+
+    public override void PaintHook()
+    {
+        G.Clear(Color.White);
+        DrawGradient(Color.FromArgb(0, 153, 255), Color.FromArgb(0, 102, 255), 0, 0, Width, 28, 90F);
+        DrawGradient(Color.FromArgb(51, 153, 255), Color.FromArgb(0, 102, 204), 0, 29, Width, 55, 90F);
+        DrawGradient(Color.White, Color.FromArgb(204, 204, 204), 0, 115, Width, Height - 55, 90F);
+        DrawGradient(Color.FromArgb(204, 204, 204), Color.White, 0, 84, Width, 35, 90F);
+        G.DrawLine(Pens.DarkBlue, 0, 28, Width, 28);
+        G.DrawLine(new Pen(new SolidBrush(Color.FromArgb(51, 204, 255))), 0, 29, Width, 29);
+        G.DrawLine(Pens.White, 0, 84, Width, 84);
+        // G.DrawString(".", this.Parent.Font, Brushes.Black, -2, Height - 12);
+        // G.DrawString(".", this.Parent.Font, Brushes.Black, Width - 5, Height - 12);
+        // DrawBorders(Pens.Black, Pens.Transparent, ClientRectangle);
+        // DrawCorners(Color.Fuchsia, ClientRectangle);
+    }
+}
